7902fdad73dd73098030cf9e56324875c6ac5f25
dkerickcivi <civinelliecga@gmail.com>
2026-06-24 15:46:09 -0300

    add meta code

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,22 @@ export default function RootLayout({
       <body className="font-sans antialiased">
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '4402851559954541');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </body>
 
       <Script id="clarity" strategy="afterInteractive">
